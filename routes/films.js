24dcbc87f3f5754212c3e35b1eb9206ec6c6e4de
GiacomoGaudino <giacomo.gaudino@yahoo.it>
2025-11-12 19:59:17 +0100

    Fix review route and configure multer for file uploads

--- routes/films.js
+++ routes/films.js
@@ -1,11 +1,13 @@
 const Router = require('express').Router()
+const multer = require('multer')
 const filmsController = require('../controllers/filmsController.js')
+const upload = multer();
 
 
 Router.get('/', filmsController.index)
 
 Router.get('/:id', filmsController.show)
 
-Router.post('/:id/review', filmsController.createReview)
+Router.post('/:id/reviews', upload.none(), filmsController.createReview)
 
 module.exports = Router
